0c4a97621729f85a02d13125c0c0960bfa4ff1ec
Tarek <103042190+tarek0201@users.noreply.github.com>
2025-07-07 17:55:28 +0200

    Update CI workflow for Docker deployment and integrate Flyway migration

--- src/main/kotlin/hs/flensburg/soop/Main.kt
+++ src/main/kotlin/hs/flensburg/soop/Main.kt
@@ -1,21 +1,17 @@
 package hs.flensburg.soop
 
-import de.lambda9.tailwind.core.KIO
 import hs.flensburg.soop.Config.Companion.parseConfig
 import hs.flensburg.soop.business.Env
 import hs.flensburg.soop.business.JEnv
 import hs.flensburg.soop.business.configureScheduling
 import hs.flensburg.soop.plugins.configureKIO
 import hs.flensburg.soop.plugins.configureSerialization
-import hs.flensburg.soop.plugins.respondKIO
 import io.github.cdimascio.dotenv.dotenv
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.server.application.Application
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.routing.get
-import io.ktor.server.routing.route
-import io.ktor.server.routing.routing
+import org.flywaydb.core.Flyway
 
 private val logger = KotlinLogging.logger { }
 
@@ -27,7 +23,14 @@ fun main(args: Array<String>) {
         else -> null
     }
 
-    val (env, _) = Env.configure(config?.parseConfig() ?: Config.parseConfig())
+    val (env, dsl) = Env.configure(config?.parseConfig() ?: Config.parseConfig())
+
+    Flyway(
+        Flyway.configure()
+            .driver("org.postgresql.Driver")
+            .dataSource(dsl)
+            .schemas("soop")
+    ).migrate()
 
     embeddedServer(
         factory = Netty,
