9d3c2c9c690e712117dec579bd4c4f7434809880
Tarek <103042190+tarek0201@users.noreply.github.com>
2025-07-30 12:57:42 +0200

    Refactor API routing to nest OpenAPI and Swagger UI under /api path

--- src/main/kotlin/hs/flensburg/marlin/plugins/Routing.kt
+++ src/main/kotlin/hs/flensburg/marlin/plugins/Routing.kt
@@ -25,9 +25,11 @@ fun Application.configureRouting(env: JEnv) {
         get(path = "/health", builder = { description = "Health check endpoint" }) {
             call.respondKIO(KIO.ok("Marlin-Backend is running!"))
         }
-        route("/api.json") { openApi() }
-        route("/swagger") { swaggerUI("/api.json") }
-        get("/") { call.respondRedirect("/swagger", permanent = false) }
+        route("/api") {
+            route("/api.json") { openApi() }
+            route("/swagger") { swaggerUI("/api/api.json") }
+            get { call.respondRedirect("/api/swagger", permanent = false) }
+        }
     }
 }
 
