0114282a9e8176a0bf7243ec453e1f0247f3fbcf
Tarek <103042190+tarek0201@users.noreply.github.com>
2025-11-09 14:58:00 +0100

    Add serialization support to BlacklistUserRequest and UpdateUserRequest data classes

--- src/main/kotlin/hs/flensburg/marlin/business/api/users/entity/BlacklistUserRequest.kt
+++ src/main/kotlin/hs/flensburg/marlin/business/api/users/entity/BlacklistUserRequest.kt
@@ -1,5 +1,8 @@
 package hs.flensburg.marlin.business.api.users.entity
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class BlacklistUserRequest(
     val userId: Long,
     val reason: String,
