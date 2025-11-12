44a63d57246085c4d6b85781fb0741fcedb39c91
Julian Müller <julian.mueller77@web.de>
2025-11-12 13:43:11 +0100

    Add user device sql

---
+++ src/main/resources/db/migration/V2025_11_12__user_device.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS marlin.user_device
+(
+    id BIGSERIAL PRIMARY KEY,
+    device_id TEXT NOT NULL,
+    fcm_token TEXT,
+    user_id BIGINT NOT NULL REFERENCES marlin.user (id) ON DELETE CASCADE,
+);
