0d2184485bdbebb0862bb0f31e390853974fa541
Julian Müller <julian.mueller77@web.de>
2025-11-20 21:36:07 +0100

    Add User Locations

---
+++ src/main/kotlin/hs/flensburg/marlin/business/api/userLocations/boundary/UserLocationsService.kt
@@ -0,0 +1,65 @@
+import de.lambda9.tailwind.core.KIO
+import de.lambda9.tailwind.core.extensions.kio.onNullFail
+import de.lambda9.tailwind.core.extensions.kio.orDie
+import hs.flensburg.marlin.business.ApiError
+import hs.flensburg.marlin.business.App
+import hs.flensburg.marlin.business.JEnv
+import hs.flensburg.marlin.business.ServiceLayerError
+import hs.flensburg.marlin.business.api.userLocations.entity.CreateOrUpdateUserLocationRequest
+import hs.flensburg.marlin.business.api.users.boundary.UserService
+import hs.flensburg.marlin.database.generated.tables.pojos.UserLocations
+
+object UserLocationsService {
+    sealed class Error(private val message: String) : ServiceLayerError {
+        object NotFound : Error("User location not found")
+        object BadRequest : Error("Bad request")
+
+        override fun toApiError(): ApiError {
+            return when (this) {
+                is NotFound -> ApiError.NotFound(message)
+                is BadRequest -> ApiError.BadRequest(message)
+            }
+        }
+    }
+
+    fun getUserLocation(id: Long): App<UserLocationsService.Error, UserLocationDTO> = KIO.comprehension {
+        UserLocationsRepo.fetchById(id).orDie().onNullFail { UserLocationsService.Error.NotFound }.map { UserLocationDTO.from(it) }
+    }
+
+    fun getAllUserLocationsFromUser(userId: Long): App<UserLocationsService.Error, List<UserLocationDTO>> = KIO.comprehension {
+        UserLocationsRepo.fetchAllByUserId(userId).orDie().onNullFail { UserLocationsService.Error.NotFound } as KIO<JEnv, UserLocationsService.Error, List<UserLocationDTO>>
+    }
+
+    fun create(
+        userLocation: CreateOrUpdateUserLocationRequest
+    ): App<UserLocationsService.Error, UserLocationDTO> = KIO.comprehension {
+        val userLocation = !UserLocationsRepo.insert(
+            UserLocations(
+                userId = userLocation.userId,
+                locationId = userLocation.locationId,
+                sentHarborNotifications = userLocation.sentHarborNotifications,
+            )
+        ).orDie()
+        UserLocationsRepo.fetchById(userLocation.id!!).orDie().onNullFail { UserLocationsService.Error.NotFound }.map { UserLocationDTO.from(it) }
+    }
+
+    fun update(
+        id: Long,
+        userLocation: CreateOrUpdateUserLocationRequest
+    ): App<UserLocationsService.Error, UserLocationDTO> = KIO.comprehension {
+        !UserLocationsRepo.update(
+            id,
+            UserLocations(
+                userId = userLocation.userId,
+                locationId = userLocation.locationId,
+                sentHarborNotifications = userLocation.sentHarborNotifications,
+            )
+        ).orDie()
+        UserLocationsRepo.fetchById(id).orDie().onNullFail { UserLocationsService.Error.NotFound }.map { UserLocationDTO.from(it) }
+    }
+
+    fun delete(id: Long): App<UserService.Error, Unit> = KIO.comprehension {
+        val userLocation = !UserLocationsRepo.fetchById(id).orDie()
+        UserLocationsRepo.deleteById(userLocation?.id!!).orDie()
+    }
+}
---
+++ src/main/kotlin/hs/flensburg/marlin/business/api/userLocations/control/UserLocationsRepo.kt
@@ -0,0 +1,61 @@
+import de.lambda9.tailwind.jooq.JIO
+import de.lambda9.tailwind.jooq.Jooq
+import hs.flensburg.marlin.business.ApiError
+import hs.flensburg.marlin.business.ServiceLayerError
+import hs.flensburg.marlin.database.generated.tables.pojos.UserLocations
+import hs.flensburg.marlin.database.generated.tables.references.USER_LOCATIONS
+
+object UserLocationsRepo {
+    sealed class Error(private val message: String) : ServiceLayerError {
+        object NotFound : Error("User Location not found")
+        object BadRequest : Error("Bad request")
+
+        override fun toApiError(): ApiError {
+            return when (this) {
+                is NotFound -> ApiError.NotFound(message)
+                is BadRequest -> ApiError.BadRequest(message)
+            }
+        }
+    }
+
+    fun insert(userLocation: UserLocations): JIO<UserLocations> = Jooq.query {
+        insertInto(USER_LOCATIONS)
+            .set(USER_LOCATIONS.USER_ID, userLocation.userId)
+            .set(USER_LOCATIONS.LOCATION_ID, userLocation.locationId)
+            .set(USER_LOCATIONS.SENT_HARBOR_NOTIFICATIONS, userLocation.sentHarborNotifications)
+            .returning()
+            .fetchInto(UserLocations::class.java).first()
+    }
+
+    fun update(
+        id: Long,
+        userLocation: UserLocations
+    ): JIO<UserLocations?> = Jooq.query {
+        update(USER_LOCATIONS)
+            .set(USER_LOCATIONS.USER_ID, userLocation.userId)
+            .set(USER_LOCATIONS.LOCATION_ID, userLocation.locationId)
+            .set(USER_LOCATIONS.SENT_HARBOR_NOTIFICATIONS, userLocation.sentHarborNotifications)
+            .where(USER_LOCATIONS.ID.eq(id))
+            .returning()
+            .fetchOneInto(UserLocations::class.java)
+    }
+
+    fun fetchById(id: Long): JIO<UserLocations?> = Jooq.query {
+        selectFrom(USER_LOCATIONS)
+            .where(USER_LOCATIONS.ID.eq(id))
+            .fetchOneInto(UserLocations::class.java)
+    }
+
+    fun fetchAllByUserId(userId: Long): JIO<List<UserLocationDTO?>> = Jooq.query {
+        selectFrom(USER_LOCATIONS)
+            .where(USER_LOCATIONS.USER_ID.eq(userId))
+            .fetchInto(UserLocations::class.java)
+            .map { UserLocationDTO.from(it) }
+    }
+
+    fun deleteById(id: Long): JIO<Unit> = Jooq.query {
+        deleteFrom(USER_LOCATIONS)
+            .where(USER_LOCATIONS.ID.eq(id))
+            .execute()
+    }
+}
