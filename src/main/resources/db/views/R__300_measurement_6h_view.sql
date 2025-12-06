0fc8bdbaa81c6d5991f585f1534cd68c8dbab5b4
Tarek <103042190+tarek0201@users.noreply.github.com>
2025-12-06 19:17:17 +0100

    Refactor continuous aggregate views to use public schema functions

--- src/main/resources/db/views/R__300_measurement_6h_view.sql
+++ src/main/resources/db/views/R__300_measurement_6h_view.sql
@@ -6,13 +6,13 @@ SELECT
     sensor_id,
     type_id,
     location_id,
-    time_bucket('6 hours', bucket) AS bucket,
-    average(rollup(stats)) AS avg,
+    public.time_bucket('6 hours', bucket) AS bucket,
+    public.average(public.rollup(stats)) AS avg,
     MIN(min) AS min,
     MAX(max) AS max,
-    num_vals(rollup(stats)) AS count,
-    stddev(rollup(stats)) AS stddev,
-    rollup(stats) AS stats
+    public.num_vals(public.rollup(stats)) AS count,
+    public.stddev(public.rollup(stats)) AS stddev,
+    public.rollup(stats) AS stats
 FROM marlin.measurement_2h_view
 GROUP BY 1,2,3,4
 WITH NO DATA;
@@ -21,7 +21,7 @@ ALTER MATERIALIZED VIEW marlin.measurement_6h_view
     SET (timescaledb.materialized_only = false);
 
 -- Refresh the 6h view every hour
-SELECT add_continuous_aggregate_policy('marlin.measurement_6h_view',
+SELECT public.add_continuous_aggregate_policy('marlin.measurement_6h_view',
        start_offset => INTERVAL '7 days',    -- Look back 7 days (ensure weekly trends are fixed)
        end_offset   => INTERVAL '6 hours',   -- Don't materialize the current incomplete 6-hour block
        schedule_interval => INTERVAL '1 hour');
